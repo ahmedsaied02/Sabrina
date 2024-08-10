28dcc2081a766f9065ed242679d144dc4ad1328b
ahmed saied <ahmed.saiedd02@gmail.com>
2024-08-10 18:02:46 +0300

    changed to pdf

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,4 @@
 import type { MetaFunction } from "@remix-run/node";
-import page1 from "~/assets/Menu_page-0001.webp"
-import page2 from "~/assets/Menu_page-0002.webp"
-import page3 from "~/assets/Menu_page-0003.webp"
-import page4 from "~/assets/Menu_page-0004.webp"
-import page5 from "~/assets/Menu_page-0005.webp"
-import page6 from "~/assets/Menu_page-0006.webp"
-import page7 from "~/assets/Menu_page-0007.webp"
-import page8 from "~/assets/Menu_page-0008.webp"
-import page9 from "~/assets/Menu_page-0009.webp"
-import page10 from "~/assets/Menu_page-0010.webp"
-import page11 from "~/assets/Menu_page-0011.webp"
-import page12 from "~/assets/Menu_page-0012.webp"
-import page13 from "~/assets/Menu_page-0013.webp"
-import page14 from "~/assets/Menu_page-0014.webp"
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -22,21 +8,15 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex flex-col gap-2b">
-      <img src={page1} alt="" />
-      <img src={page2} alt="" />
-      <img src={page3} alt="" />
-      <img src={page4} alt="" />
-      <img src={page5} alt="" />
-      <img src={page6} alt="" />
-      <img src={page7} alt="" />
-      <img src={page8} alt="" />
-      <img src={page9} alt="" />
-      <img src={page10} alt="" />
-      <img src={page11} alt="" />
-      <img src={page12} alt="" />
-      <img src={page13} alt="" />
-      <img src={page14} alt="" />
+    <div className="flex">
+       <div style={{ width: '100%', height: '100vh' }}>
+      <iframe
+        src="/menu.pdf"
+        width="100%"
+        height="100%"
+        style={{ border: 'none' }}
+      />
+    </div>
     </div>
   );
 }
