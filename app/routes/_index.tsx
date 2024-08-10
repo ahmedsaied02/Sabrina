632bfe605c2b4217cdcf31190a711f958c7321f3
ahmed saied <ahmed.saiedd02@gmail.com>
2024-08-10 19:36:05 +0300

    bahaa simplicity

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,22 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { redirect, type MetaFunction } from "@remix-run/node";
 
-export default function Index() {
-  return (
-    <div className="flex">
-       <div style={{ width: '100%', height: '100vh' }}>
-      <iframe
-        src="/menu.pdf"
-        width="100%"
-        height="100%"
-        style={{ border: 'none' }}
-      />
-    </div>
-    </div>
-  );
+
+export function loader(){
+  return redirect("/menu.pdf")
 }
